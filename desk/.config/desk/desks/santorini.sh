57077065ca86c693fb694d7a03223de60947accd
John Fresco <john.fresco@utah.edu>
2019-03-18 19:24:13 -0600

    update

---
+++ desk/.config/desk/desks/santorini.sh
@@ -0,0 +1,5 @@
+alias cdproj="cd ~/class/fp/santorini"
+alias repl="cd ~/class/fp/santorini; dune utop src/lib -- -require rtop; cd -"
+
+cd ~/class/fp/santorini
+
---
+++ zsh/.config/zsh/keys.zsh
@@ -0,0 +1,10 @@
+# 
+#
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+# bindkey '^r' history-incremental-search-backward
